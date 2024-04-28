dd45fc7e4552c1fc175eb0b104c2be1ff483a6ff
KundaAndrii <145130888+Octopus663@users.noreply.github.com>
2024-04-28 18:49:54 +0300

    Initial commit

---
+++ src/main/java/com/example/rentapart/RentApartApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentapart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentApartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentApartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentapart/RentApartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentapart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentApartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
